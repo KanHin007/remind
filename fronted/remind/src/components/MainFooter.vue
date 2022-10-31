41845da4d7976a3a7c3ac1be3c947b36923974fb
韩彬 <24545955+KanHin007@users.noreply.github.com>
2022-10-31 19:56:40 +0800

    first commit

---
+++ fronted/remind/src/components/MainFooter.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="footer">
+    <el-tabs tab-position="bottom" @tab-click="selectMenu" stretch="true">
+      <el-tab-pane v-for="(perItem,id) in tabList" :key="id" :label="perItem.tabName" style="width: 100vh" >
+      </el-tab-pane>
+    </el-tabs>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MainFooter",
+  data() {
+    return{
+      tabList : [{
+        id : '1',
+        tabName :'首页',
+      },{
+        id :'2',
+        tabName: '提醒'
+      },{
+        id : '3',
+        tabName: '我的'
+      }],
+
+    }
+  },
+  components:{
+
+  },
+  mounted() {
+  },
+  computed(){
+  },
+  created() {
+  },
+  methods:{
+    selectMenu (tab) {
+      if(tab.index === '1'){
+        console.log()
+      }else if (tab.index === '2'){
+        console.log()
+      }else {
+        console.log()
+      }
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.footer{
+  width: 100%;
+  bottom: 0;
+  margin: auto;
+  /*position: absolute;*/
+  text-align: center;
+}
+
+>>> .el-tabs__nav .el-tabs__item {
+
+
+}
+</style>
